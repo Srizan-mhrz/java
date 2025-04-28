654c39c45d481c0e9d21721671d62c24b92662f5
Srizan-mhrz <shrijan.mhrz@gmail.com>
2025-04-28 08:58:42 +0545

    week5

---
+++ whileLoop.java
@@ -0,0 +1,51 @@
+public class whileLoop {
+    public static void main(String[]args){
+        // int i=1;
+        // while (i<=10){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        // int j=2;
+        // do {
+        //     System.out.println(j);
+        //     j+=2;
+        // }while(j<=10);
+//wap using while loop to print 1 to 100
+        // int i=0;
+        // while(i<=100){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+//multiplication table using do while loop
+        // int i=1;
+        // int j=1;
+        // do {
+        //     System.out.println(i+"*"+j+"="+(i*j));
+        //     j++;
+
+        // }while(j<=10);
+//wap to count the digits of a number using while loop
+    // int a=234;
+    // int count=0;
+    // while(a!=0){
+    //     a=a/10;
+    //     count++;
+
+    // }   
+    // System.out.println("count of digits is: "+count);
+    
+    
+//wap to reverse a number using while loop
+    // int a =1234;
+    // int rev=0;
+    // while(a!=0){
+    //     int digit=a%10;
+    //     rev=rev*10+digit;
+    //     a=a/10;
+    // }
+    // System.out.println("reverse of number is: "+rev);
+    }
+    
+}
